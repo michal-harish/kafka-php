89572054e6dec5876bdd2c73f2ea86ef8eb8f3e1
michalharis <michalharis@4f6741ee-d1fe-45f4-8443-8b850bfd67de>
2012-07-31 00:51:13 +0000

    Reprocessing failed messages in consumer example.
    git-svn-id: http://xp-dev.com/svn/kafkaphp/trunk@25 4f6741ee-d1fe-45f4-8443-8b850bfd67de

--- scripts/consumer.php
+++ scripts/consumer.php
@@ -36,66 +36,75 @@ if (!$topic)
 
 //connection
 $kafka = new Kafka($kafkaHost, $kafkaPort);
+
 //consumer
 $consumer = $kafka->createConsumer();
+
 //offsets request
 echo "\nOFFSETS REQUEST\n\n";
 foreach($consumer->offsets($topic, 0) as $offsetItem )
 {
     echo $offsetItem . "\n";
 }
+
 //initialize watermark offset
 $watermark = new Kafka_Offset($offsetHex);
 //fetch request
-$processed = 0;
-while(TRUE)
-{	
-	echo "\nFETCH REQUEST FROM WATERMARK OFFSET: $watermark\n";
-	if ($consumer->fetch($topic, 0, $watermark))
-	{
-		try {			
-			$consistent = TRUE;
-		    while ($message = $consumer->nextMessage())
-		    {
-		    	//no state has been modified yet, but there can be errors when fetching etc.		    			    	
-		    	if (rand(0,20) == 0)
-		    	{
-		    		throw new Exception('Simulation of consumer failure in a consistent state.');
-		    	}
-		    	/*****************************************************************************
-		    	 * Start processing the message.. */
-		    	$consistent = FALSE;
-		    	if (rand(0,20) == 0)
-		    	{
-		    		throw new Exception('Simulation of consumer failure - THE ERROR OCCURED WHILE THERE IS AN INCONSISTENT STATE CREATED BY PARTIALLY PROCESSED MESSAGE!');
-		    	}		    	
-		    	//if processing went fine, we get the new new watermark, but it's not over yet 		    	
-		    	$watermark = $consumer->getWatermark();		    			    
-		    	/* Now store the watermark somerwhere..
-		    	** Only when the new watermark has been stored, we're consistent again
-		    	** and we can consider the last message 'processed' 
-		    	******************************************************************************/		    	 
-		    	$consistent = TRUE;
-		    	$processed++;		    		    	
-		        echo "\n[offset:" . $message->offset() . " watermark: $watermark] " . $message->payload() ;		        
-		    }		    
-		    echo "\nNO MORE MESSAGES, TOTAL PROCESSED MESSAGES: $processed, NEW WATERMARK: " . $watermark . "\n\n";
-		    //if there's no more messages without error then it's the last message in the topic
-		    break;
-	   	} catch (Exception $e)
-	   	{
-	   		echo "\nERROR PROCESSING MESSAGE AT OFFSET $watermark: " . $e->getMessage();
-	   		if (!$consistent)
-	   		{
-	   			/******************************************************************************
-	   			* compensate for the inconsistency, e.g. by reversing what was modified
-	   			******************************************************************************/ 
-	   		}
-	   		echo "\n";
-	   	}   	
-	   	usleep(100);
-	}
-}	
+$totalProcessed = 0;
+echo "\nFETCH REQUEST FROM WATERMARK OFFSET: $watermark\n";
+if ($consumer->fetch($topic, 0, $watermark))
+{
+	try {			
+	    while ($message = $consumer->nextMessage())
+	    {
+	    	$consistent = TRUE;
+	    	$processed = FALSE;
+	    	do {
+		    	try {
+			    	//no state has been modified yet, but there can be errors when fetching etc.		    			    	
+			    	if (rand(0,100) == 0)
+			    	{
+			    		throw new Exception('Simulation of consumer failure in a consistent state.');
+			    	}
+			    	/*****************************************************************************
+			    	 * Start processing the message.. */
+			    	$consistent = FALSE;
+			    	if (rand(0,200) == 0)
+			    	{
+			    		throw new Exception('Simulation of consumer failure - THE ERROR OCCURED WHILE THERE IS AN INCONSISTENT STATE CREATED BY PARTIALLY PROCESSED MESSAGE!');
+			    	}		    	
+			    	//if processing went fine, we get the new new watermark, but it's not over yet 		    	
+			    	$watermark = $consumer->getWatermark();		    			    
+			    	/* Now store the watermark somerwhere..
+			    	** Only when the new watermark has been stored, we're consistent again
+			    	** and we can consider the last message 'processed' 
+			    	******************************************************************************/		    	 
+			    	$consistent = TRUE;
+			    	$processed = TRUE;
+			        echo "\n[offset:" . $message->offset() . " watermark: $watermark] " . $message->payload() ;
+		    	} catch (Exception $e)
+			   	{
+			   		echo "\nERROR PROCESSING MESSAGE AT OFFSET $watermark: " . $e->getMessage();
+			   		if (!$consistent)
+			   		{
+			   			/******************************************************************************
+			   			* compensate for the inconsistency, e.g. by reversing what was modified
+			   			******************************************************************************/
+			   			$consistent = TRUE;
+			   		}
+			   		echo "\n";
+			   	}   
+	    	} while(!$processed);		        
+		   	$totalProcessed++;
+	    }		    
+	    echo "\nNO MORE MESSAGES, TOTAL PROCESSED MESSAGES: $totalProcessed, NEW WATERMARK: " . $watermark . "\n\n";
+   	} 
+   	catch (Exception $e)
+   	{
+   		echo "\nERROR FETCHING MESSAGE AT OFFSET $watermark: " . $e->getMessage();
+   		
+   	}   	
+}
 
 //go home
 $consumer->close();

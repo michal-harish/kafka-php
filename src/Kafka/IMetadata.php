d656a7aa42ac935db7526d3c8fcc25ab7f1542aa
Michal Haris <michal.haris@visualdna.com>
2012-12-12 15:17:55 +0000

    IMetadata and Metadata implementation for 0.7 and ConsumerConnector Facade.

---
+++ src/Kafka/IMetadata.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Kafka;
+
+interface IMetadata
+{
+    function __construct($connectionString);
+
+    /**
+     * @return array[<topic>][<virutalPartition>] = array('broker'= <brokerId>, 'partition' = <brokerPartition>)
+     */
+    function getTopicMetadata();
+
+    /**
+     * @param int $brokerId
+     * @return array('name'=> ..., 'host'=>..., 'port' =>... )
+     */
+    function getBrokerInfo($brokerId);
+
+}
